996b2f749f853c782fd94c96ffdbfe1dd3ec13c9
Ivan Dorofeev <axxel123@rambler.ru>
2022-07-28 10:13:41 +0300

    Добавил 2 фукнции: - Скачивание картинки по url - Парсинг spacexdata и поиск url картинки

---
+++ main.py
@@ -0,0 +1,29 @@
+import os.path
+
+import requests
+
+
+def download_img(url, path_to_save):
+    if not os.path.exists(path_to_save):
+        os.makedirs(path_to_save)
+
+    name_file_from_url = url.split('/')[-1]
+    filename = os.path.join(path_to_save, name_file_from_url)
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+def get_links():
+    response = requests.get('https://api.spacexdata.com/v5/launches')
+    for i in response.json()[:5]:
+        img_url = i['links']['patch']['large']
+        download_img(img_url, 'img')
+
+
+if __name__ == '__main__':
+    # download_img('https://static.djangoproject.com/img/fundraising-heart.cd6bb84ffd33.svg', 'images')
+    get_links()
