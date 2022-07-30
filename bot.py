c846b1b4054e562246f55eea7e6c4878d9b86e83
Ivan Dorofeev <axxel123@rambler.ru>
2022-07-30 09:24:54 +0300

    Добавил ввод интервала отправки через argparse

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+import random
 import time
 
 import telegram
@@ -7,17 +9,28 @@ from telegram import InputMediaDocument
 
 load_dotenv()
 
-tg_token = os.environ['TG_BOT_TOKEN']
-
-IvanDorofeev20 = 286730091
-sky_images_bot = -1001633240720
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Отправляет картинки из папки /image в телеграмм канал '
+    )
+    parser.add_argument(
+        'hours',
+        help='Частота повтора отправки (в часах)',
+        type=int
+    )
+    args = parser.parse_args()
+
+    tg_token = os.environ['TG_BOT_TOKEN']
+    chat_id = os.environ['SKY_iIMAGES_CHANNEL_CHAT_ID']
+
     bot = telegram.Bot(token=tg_token)
 
+    files_list = []
     while True:
-        for dirpath, dirnames, filenames in os.walk(os.path.join(os.getcwd(), 'images')):
-            for filename in filenames:
-                send_img = bot.send_media_group(chat_id=sky_images_bot, media=[
-                    InputMediaDocument(media=open(f'images/{filename}', 'rb'))])
-                time.sleep(60 * 60 * 4)
+        if not files_list:
+            for dirpath, dirnames, filenames in os.walk(os.path.join(os.getcwd(), 'images')):
+                files_list = filenames
+        send_img = bot.send_media_group(chat_id=chat_id,
+                                        media=[InputMediaDocument(media=open(f'images/{files_list}', 'rb'))])
+        time.sleep(60 * 60 * args.hours)
+        random.shuffle(files_list)
