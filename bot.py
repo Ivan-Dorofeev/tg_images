2b6b95ad1bd15d1b262b519d48aa0bec7f52f563
Ivan Dorofeev <axxel123@rambler.ru>
2022-07-30 08:22:05 +0300

    publish photo to Telegram channel.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 
 import telegram
 from dotenv import load_dotenv
+from telegram import InputMediaDocument
 
 load_dotenv()
 
@@ -15,5 +16,8 @@ bot = telegram.Bot(token=tg_token)
 # print(bot.get_updates())
 # for i in bot.get_updates():
 #   print(i['my_chat_member'])
-mess = bot.send_message(chat_id=sky_images_bot, text="Привет, тут сообщение")
+mess = bot.send_message(chat_id=sky_images_bot, text="Лови картинку")
+img = bot.send_media_group(chat_id=sky_images_bot, media=[InputMediaDocument(media=open('spacex_images/42374725204_dae09db889_o.jpg', 'rb'))])
+
 print(mess)
+print(img)
