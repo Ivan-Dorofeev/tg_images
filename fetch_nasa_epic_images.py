11b107883c83e8fcff730597bfde4a1d292feea9
Ivan Dorofeev <axxel123@rambler.ru>
2022-07-30 14:22:02 +0300

    Упростил строку

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -27,8 +27,9 @@ def fetch_nasa_epic_images():
     url = 'https://api.nasa.gov/EPIC/api/natural'
     response = requests.get(url, params={'api_key': nasa_api_key, 'date': datetime.datetime.today()})
     for image_info in response.json()[:5]:
-        date_and_name_img = image_info['date'].split(" ")[0].replace('-', '/') + '/png/' + image_info['image'] + '.png'
-        response_img = requests.get(f'https://api.nasa.gov/EPIC/archive/natural/{date_and_name_img}',
+        date_img = image_info['date'].split(" ")[0].replace('-', '/')
+        name_img = '/png/' + image_info['image'] + '.png'
+        response_img = requests.get(f'https://api.nasa.gov/EPIC/archive/natural/{date_img}{name_img}',
                                     params={'api_key': nasa_api_key})
         img_url = response_img.url
         download_img(img_url, 'images')
