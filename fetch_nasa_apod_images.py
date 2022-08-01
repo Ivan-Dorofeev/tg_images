0f83c6b2be5e2be8523cc7c9dce4cc7d31d6519e
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-01 07:53:52 +0300

    Добави нейминг к числам

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -6,10 +6,11 @@ from process_img import download_img
 
 
 def fetch_nasa_apod_images():
+    images_count = 5
     load_dotenv()
     nasa_api_key = os.environ['NASA_API_KEY']
     url = 'https://api.nasa.gov/planetary/apod?'
-    response = requests.get(url, params={'api_key': nasa_api_key, 'count': 5})
+    response = requests.get(url, params={'api_key': nasa_api_key, 'count': images_count})
     response.raise_for_status()
     img_urls = response.json()
     for img_url in img_urls:
