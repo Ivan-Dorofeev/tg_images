1f65e1d2a35b9164aa27acd7c43b975fe7c958d5
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-01 08:44:52 +0300

    Подсушил код

--- process_img.py
+++ process_img.py
@@ -4,10 +4,7 @@ from urllib.parse import unquote, urlparse
 import requests
 
 
-def download_img(url, path_to_save):
-    if not os.path.exists(path_to_save):
-        os.makedirs(path_to_save)
-
+def download_img(url, path_to_save='media'):
     *_, file_from_url = urlparse(url).path.split('/')
     img_name, img_extension = file_from_url.split('.')
     unquote_img_name = unquote(img_name)
