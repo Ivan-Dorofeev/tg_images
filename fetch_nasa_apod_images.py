7bbbf1f4c0795a059935dc2f34dac51e7e79a126
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-03 08:02:45 +0300

    Подсушил код

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -9,7 +9,7 @@ def fetch_nasa_apod_images():
     images_count = 5
     load_dotenv()
     nasa_api_key = os.environ['NASA_API_KEY']
-    url = 'https://api.nasa.gov/planetary/apod?'
+    url = 'https://api.nasa.gov/planetary/apod'
     response = requests.get(url, params={'api_key': nasa_api_key, 'count': images_count})
     response.raise_for_status()
     img_urls = response.json()
